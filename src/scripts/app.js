23ae0ab50b2198b1143518dc8d99248ca8496ab0
Leslie Restrepo <leslierestrepo@gmail.com>
2016-02-11 15:31:20 +0400

    Integration with Firebase

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,93 +1,27 @@
-var app = angular.module("app", ['ngRoute']);
+var app = angular.module("app", ['ngRoute','firebase']);
+
 
 app.config(['$routeProvider', function ($routeProvider){
 
  	$routeProvider
  		.when('/', {
      		templateUrl:'scripts/views/home.html',
-     		controller: 'listController'
+     		controller: 'HomeController'
   		})
   		.when('/contact/new', {
    	 		templateUrl: 'scripts/views/newContact.html',
-     		controller: 'contactController'
+     		controller: 'ContactController'
   		})
  		.when('/contact/:id', {
    	 		templateUrl: 'scripts/views/showContact.html',
-     		controller: 'contactController'
+     		controller: 'ContactController'
   		})
   		.when('/contact/edit/:id', {
    	 		templateUrl: 'scripts/views/editContact.html',
-     		controller: 'contactController'
+     		controller: 'ContactController'
   		})  		
 	  	.otherwise ({
 	     	templateUrl:'scripts/views/home.html'
 	  	});
 
 }]);
-
-app.controller("listController", ['$scope','$routeParams','$location','$window', function($scope,$routeParams,$location,$window){
-	$scope.contacts = contactsList;
-}]);
-
-app.controller("contactController", ['$scope','$routeParams','$location','$window', function($scope,$routeParams,$location,$window){
-	$scope.contacts = contactsList;
-	$scope.contact = $scope.contacts[$routeParams.id];
-	$scope.showContact = function(id){
-		$scope.contact = $scope.contacts[id]; //$scope.contacts[$routeParams.id] || 
-	};
-	$scope.editarContact = function(contact){
-		$scope.contacts[getPosition(contact.id)] = contact;
-		$location.url('/#');
-	};
-	$scope.deleteContact = function(id){
-		if(confirm("Are you sure?")){
-			$scope.contacts.splice(getPosition(id),1);
-			$location.url('/#');
-			//$location.path('/');
-			//$location.url('/#home.html/');
-			//$location.path('//');
-			//window.location = "#/";
-		    //$scope.$apply();
-			//$window.location.href = '/home.html/';
-			//$window.location.href= "#/home.html";
-		}
-	};
-	$scope.saveContact = function(contact){
-		$scope.contacts.push(contact);
-		$location.url('/#');
-	};
-
-	getPosition = function(id){
-		for(var i = 0; i < $scope.contacts.length; i++) {
-
-		    if($scope.contacts[i].id == id) {
-		        return i;
-		    }
-		}
-	};
-
-}]);
-
-var contactsList = [
-	{
-		id:0,
-		name:"George",
-		email:"gg@gmail.com",
-		phone:"1234",
-		notes:"test george"
-	},
-	{
-		id:1,
-		name:"Anu",
-		email:"aa@aa.com",
-		phone:"456",
-		notes:"test anu"
-	},
-	{
-		id:2,
-		name:"Lewis",
-		email:"ll@ll.com",
-		phone:"789",
-		notes:"test lewis"
-	},
-];
